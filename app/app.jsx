e5fc86dfdf64d6e978e2cfef6f695c39f6508442
Camera <camerawalrond@gmail.com>
2019-09-03 17:35:19 -0400

    setting up react environment

---
+++ app/app.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    // this.state = {
+    //   Launch: false,
+    // }
+}
+
+/* Needs: functions on the constructor for each button click. 
+Each on of those functions to be referneced in the super fuction using the bind method
+*/
+
+
+  render(){
+  return(
+<div>
+  Hey
+</div>
+
+  )
+  }
+}
